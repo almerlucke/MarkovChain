85eb19385ecaf7b78496c0bdc704bd0ef53b7800
Almer Lucke <almerlucke@Almers-MacBook-Pro.local>
2013-05-07 18:37:19 +0200

    Initial Commit

---
+++ MarkovChains/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MarkovChains
+//
+//  Created by Almer Lucke on 5/7/13.
+//  Copyright (c) 2013 Farcoding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
